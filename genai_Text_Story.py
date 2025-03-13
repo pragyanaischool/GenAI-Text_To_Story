bf817ca4e5c09e54a73d05b937c41b5d94c9aa56
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-13 06:37:36 +0530

    Update genai_Text_Story.py

--- genai_Text_Story.py
+++ genai_Text_Story.py
@@ -10,7 +10,17 @@ from langchain.chat_models import ChatGroq  # Using ChatGroq instead of ChatOpen
 from langchain.prompts import PromptTemplate
 from transformers import pipeline
 
-from utils.custom import css_code
+css_code: str = """
+    <style>
+    section[data-testid="stSidebar"] > div > div:nth-child(2) {
+        padding-top: 0.75rem !important;
+    }
+    
+    section.main > div {
+        padding-top: 64px;
+    }
+    </style>
+"""
 
 load_dotenv(find_dotenv())
 HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
